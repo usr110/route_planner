edd7404d9d53da3b1de90b463102b8f3587e568e
usr110 <m.aliabbas@gmail.com>
2017-04-12 17:33:12 +0100

    Save route time, distance and elevation to the dataset

--- process_data.R
+++ process_data.R
@@ -6,9 +6,20 @@ dat <- read.csv("GpsTrips.csv", header = T, as.is = T, stringsAsFactors = F)
 dat_bike <- filter(dat, travelmode == 2)
 # Subset walking trips
 dat_walk <- filter(dat, travelmode == 1)
+
 # Test route
-start_point <- c(dat_bike$startlong[1], dat_bike$startlat[1])
-end_point <- c(dat_bike$endlong[1], dat_bike$endlat[1])
-rdata <- route_graphhopper(start_point, end_point, vehicle = "bike")
+for (i in 1:nrow(dat_bike)){
+  start_point <- c(dat_bike$startlong[i], dat_bike$startlat[i])
+  end_point <- c(dat_bike$endlong[i], dat_bike$endlat[i])
+  cat(i, " - ", start_point, " - ", end_point, "\n")
+  if ( !i %in% c(42, 50, 66)){#!identical(start_point, end_point))
+    rdata <- route_graphhopper(start_point, end_point, vehicle = "bike")
+    # "time"        "dist"        "change_elev"
+    dat_bike$time[i] <- rdata$time
+    dat_bike$dist[i] <- rdata$dist
+    dat_bike$change_elev[i] <- rdata$change_elev
+  }
+}
+# 
 
 
